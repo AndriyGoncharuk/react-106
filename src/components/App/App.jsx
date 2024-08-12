a796d54e9857280e2f8c84e45ca6f9d16f5cf726
Alexander Repeta <alexander.repeta@gmail.com>
2024-08-12 21:00:16 +0300

    add examples

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,65 @@
-// import articles from "../../articles.json";
+import { useState } from "react";
+// import ClicksTracker from "../ClicksTracker/ClicksTracker";
+// import Toggler from "../Toggler/Toggler";
+import Reader from "../Reader/Reader";
+import articles from "../../articles.json";
 import css from "./App.module.css";
 
 export default function App() {
+  const [clicks, setClicks] = useState(0);
+  const [values, setValues] = useState({
+    a: 1,
+    b: 2,
+    c: 3,
+  });
+
+  const updateValues = () => {
+    setValues({
+      ...values,
+      b: 5,
+    });
+  };
+
+  const udpateClicks = () => {
+    setClicks(clicks + 1);
+  };
+
+  const resetClicks = () => {
+    setClicks(0);
+  };
+
   return (
     <div className={css.container}>
       <h1>State in React</h1>
 
-      {/* <Reader items={articles} /> */}
+      <Reader items={articles} />
+
+      {/* <button onClick={updateValues}>
+        Update values {values.a + values.b + values.c}
+      </button> */}
+
+      {/* <Toggler>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
+          ratione architecto voluptas illum eaque quae natus quidem illo
+          perferendis, quia tempore deserunt animi vero? Assumenda ipsam
+          cupiditate odit illo soluta?
+        </p>
+      </Toggler> */}
+
+      {/* <Toggler>
+        <h2>Omagad</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit illo
+          vitae officiis perferendis quod nisi amet architecto nam, non magnam.
+        </p>
+      </Toggler> */}
+
+      <hr />
+      {/* <ClicksTracker value={clicks} onUpdate={udpateClicks} />
+      <ClicksTracker value={clicks} onUpdate={udpateClicks} />
+      <button onClick={resetClicks}>Reset clicks</button> */}
+      <hr />
     </div>
   );
 }
--- src/components/Reader/Reader.jsx
+++ src/components/Reader/Reader.jsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
+import css from "./Reder.module.css";
+
 export default function Reader({ items }) {
+  const [index, setIndex] = useState(0);
+
+  const handlePrev = () => {
+    setIndex(index - 1);
+  };
+
+  const handleNext = () => {
+    setIndex(index + 1);
+  };
+
+  const currentItem = items[index];
+  const isFirst = index === 0;
+  const isLast = index === items.length - 1;
+
   return (
     <div>
-      <header>
+      <header className={css.header}>
         <div>
-          <button>Prev</button>
-          <button>Next</button>
+          <button onClick={handlePrev} disabled={isFirst}>
+            Prev
+          </button>
+          <button onClick={handleNext} disabled={isLast}>
+            Next
+          </button>
         </div>
-
-        <p>0/0</p>
+        <p>
+          {index + 1}/{items.length}
+        </p>
       </header>
 
       <article>
-        <h2>Topic</h2>
-        <p>Text</p>
+        <h2>{currentItem.topic}</h2>
+        <p>{currentItem.text}</p>
       </article>
     </div>
   );
