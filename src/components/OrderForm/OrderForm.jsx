2fee44cf494a65dc06f6d28935fc2bc32aec0043
Alexander Repeta <alexander.repeta@gmail.com>
2024-08-19 20:57:08 +0300

    add examples

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,22 +1,47 @@
-import { useState } from "react";
+import { useId, useState } from "react";
 import { FaTshirt } from "react-icons/fa";
 import css from "./OrderForm.module.css";
 
 const initialValues = {
-  size: "md",
-  color: "blue",
+  size: "sm",
+  color: "red",
+  comment: "",
 };
 
-export default function OrderForm() {
-  const [state, setState] = useState(initialValues);
+export default function OrderForm({ onOrder }) {
+  const id = useId();
+  const [values, setValues] = useState(initialValues);
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onOrder(values);
+    setValues(initialValues);
+  };
 
   return (
-    <form autoComplete="off" noValidate className={css.form}>
-      <FaTshirt size="160" color={state.color} />
+    <form
+      autoComplete="off"
+      noValidate
+      className={css.form}
+      onSubmit={handleSubmit}
+    >
+      <FaTshirt size="160" color={values.color} />
 
       <div className={css.group}>
-        <label>Size</label>
-        <select name="size">
+        <label htmlFor={`${id}-size`}>Size</label>
+        <select
+          name="size"
+          value={values.size}
+          onChange={handleChange}
+          id={`${id}-size`}
+        >
           <option value="sm">Small</option>
           <option value="md">Medium</option>
           <option value="lg">Large</option>
@@ -24,15 +49,51 @@ export default function OrderForm() {
       </div>
 
       <div className={css.group}>
-        <label>Color</label>
-        <select name="color">
+        <label htmlFor={`${id}-color`}>Color</label>
+        <select
+          name="color"
+          value={values.color}
+          onChange={handleChange}
+          id={`${id}-color`}
+        >
           <option value="red">Red</option>
           <option value="green">Green</option>
           <option value="blue">Blue</option>
         </select>
       </div>
 
+      <div className={css.group}>
+        <label htmlFor={`${id}-comment`}>Comment</label>
+        <textarea
+          name="comment"
+          value={values.comment}
+          onChange={handleChange}
+          id={`${id}-comment`}
+        ></textarea>
+      </div>
+
       <button type="submit">Submit</button>
     </form>
   );
 }
+
+// const handleChangeSize = (event) => {
+//   setValues({
+//     ...values,
+//     size: event.target.value,
+//   });
+// };
+
+// const handleChangeColor = (event) => {
+//   setValues({
+//     ...values,
+//     color: event.target.value,
+//   });
+// };
+
+// const handleChangeComment = (event) => {
+//   setValues({
+//     ...values,
+//     comment: event.target.value,
+//   });
+// };
